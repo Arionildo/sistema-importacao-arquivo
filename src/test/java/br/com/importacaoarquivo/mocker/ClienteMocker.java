e216971fae4f3fffc39eeea6cb42dbc99b06446a
ArionildoEscouto <arionildo6@gmail.com>
2020-09-23 19:19:30 -0300

    Add Mocker for Tests

---
+++ src/test/java/br/com/importacaoarquivo/mocker/ClienteMocker.java
@@ -0,0 +1,12 @@
+package br.com.importacaoarquivo.mocker;
+
+public class ClienteMocker {
+
+    public static String[] criarUmaLinha() {
+        return new String[]{"002ç2345675434544345çJose da SilvaçRural"};
+    }
+
+    public static String[] criarDuasLinhas() {
+        return new String[]{"002ç2345675434544345çJose da SilvaçRural", "002ç2345675433444345çEduardo PereiraçRural"};
+    }
+}
