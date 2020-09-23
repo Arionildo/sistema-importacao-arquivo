6d475ce1748d4adb776b743673da206b37cfac30
ArionildoEscouto <arionildo6@gmail.com>
2020-09-23 18:40:59 -0300

    Add Configuração Inicial Projeto

---
+++ src/test/java/br/com/sistemaimportacaoarquivo/SistemaimportacaoarquivoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sistemaimportacaoarquivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaimportacaoarquivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
